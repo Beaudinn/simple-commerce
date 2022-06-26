7c117886eba8cabe779f9f8554b58943fa953f10
Beaudinn Greve <beaudinngreve@gmail.com>
2022-06-26 12:10:00 +0200

    update

--- src/Orders/EloquentOrderRepository.php
+++ src/Orders/EloquentOrderRepository.php
@@ -19,7 +19,7 @@ class EloquentOrderRepository implements RepositoryContract
 	protected $model;
 
 	protected $knownColumns = [
-		'id', 'state', 'is_paid', 'is_shipped', 'reference', 'is_refunded', 'items', 'grand_total', 'items_total', 'tax_total',
+		'id', 'state', 'is_paid', 'is_shipped', 'reference', 'is_refunded', 'items', 'upsells', 'grand_total', 'items_total', 'tax_total',
 		'shipping_total', 'coupon_total', 'shipping_name', 'shipping_address', 'shipping_address_line2',
 		'shipping_city', 'shipping_postal_code', 'shipping_region', 'shipping_country', 'billing_name',
 		'billing_address', 'billing_address_line2', 'billing_city', 'billing_postal_code', 'billing_region',
@@ -208,12 +208,12 @@ class EloquentOrderRepository implements RepositoryContract
 		$model->paid_date = $order->get('paid_date');
 
 		$model->save();
-		var_dump($order->upsells()->map->toArray(), $model->upsells);
-
-		if( $model->upsells){
-			var_dump($model->id);
-			die();
-		}
+		//var_dump('--------',$order->upsells()->map->toArray(), $model->upsells);
+		//
+		//if( $model->upsells){
+		//	var_dump('--------',$model->id);
+		//	die();
+		//}
 
 		$order->id = $model->id;
 		$order->state = $model->state;
