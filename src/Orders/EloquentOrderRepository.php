1e54c1fbc4aea8b2f08a167a879b53d767768e89
Beaudinn Greve <beaudinngreve@gmail.com>
2022-06-26 11:42:50 +0200

    update

--- src/Orders/EloquentOrderRepository.php
+++ src/Orders/EloquentOrderRepository.php
@@ -145,7 +145,7 @@ class EloquentOrderRepository implements RepositoryContract
 		}
 
 
-		var_dump($order->upsells()->map->toArray()); die();
+		var_dump($order->upsells()); die();
 		$model->locale = $order->locale();
 		$model->is_paid = $order->isPaid();
 		$model->order_number = $order->orderNumber();
