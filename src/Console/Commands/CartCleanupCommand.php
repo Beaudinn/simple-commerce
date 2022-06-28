240e6a2a68080dd182ffee147134d7b46999dfd2
Beaudinn Greve <beaudinngreve@gmail.com>
2022-06-28 16:46:23 +0200

    update

--- src/Console/Commands/CartCleanupCommand.php
+++ src/Console/Commands/CartCleanupCommand.php
@@ -12,27 +12,27 @@ class CartCleanupCommand extends Command
 {
     use RunsInPlease;
 
-    protected $name = 'sc:cart-cleanup';
+    protected $name = 'sc:cart-cleanup {days}';
     protected $description = 'Cleanup carts older than 14 days.';
 
     public function handle()
     {
         $this->info('Cleaning up..');
 
-        if (isset(SimpleCommerce::orderDriver()['collection'])) {
-            Entry::whereCollection(SimpleCommerce::orderDriver()['collection'])
-                ->where('is_paid', false)
-                ->filter(function ($entry) {
-                    return $entry->date()->isBefore(now()->subDays(14));
-                })
-                ->each(function ($entry) {
-                    $this->line("Deleting order: {$entry->id()}");
-
-                    $entry->delete();
-                });
-
-            return;
-        }
+        //if (isset(SimpleCommerce::orderDriver()['collection'])) {
+        //    Entry::whereCollection(SimpleCommerce::orderDriver()['collection'])
+        //        ->where('is_paid', false)
+        //        ->filter(function ($entry) {
+        //            return $entry->date()->isBefore(now()->subDays(14));
+        //        })
+        //        ->each(function ($entry) {
+        //            $this->line("Deleting order: {$entry->id()}");
+		//
+        //            $entry->delete();
+        //        });
+		//
+        //    return;
+        //}
 
         if (isset(SimpleCommerce::orderDriver()['model'])) {
             $orderModelClass = SimpleCommerce::orderDriver()['model'];
@@ -41,7 +41,7 @@ class CartCleanupCommand extends Command
                 ->query()
 	            ->whereState('state', Draft::class)
                 ->where('is_paid', false)
-                ->where('created_at', '<', now()->subDays(14))
+                ->where('created_at', '<', now()->subDays($this->argument('days') ?? 14))
                 ->each(function ($model) {
                     $this->line("Deleting order: {$model->id}");
 
